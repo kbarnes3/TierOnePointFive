82adffc02a89739c7cabc1a97221208a7abce041
Kevin Barnes <kbarnes3@gmail.com>
2017-06-20 18:24:56 -0700

    Adding boilerplate console app

---
+++ python/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+setuptools.setup(name='tieronepointfive',
+      version='0.1.0',
+      description='Program meant to automatically reboot a cable modem and router when a network outage is detected',
+      author='Kevin Barnes',
+      author_email='kbarnes3@gmail.com',
+      url='https://github.com/kbarnes3/TierOnePointFive',
+      license='Other',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development',
+          'License :: Other/Proprietary License',
+          "Operating System :: OS Independent",
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          ],
+      install_requires=[],
+      packages=['tieronepointfive', ],
+      entry_points={
+          'console_scripts': [
+              'tieronepointfive = tieronepointfive.console:run',
+          ]
+      },
+      )
