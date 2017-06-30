cb251c5102d46cc42f0ca689703197cd9c5b4d47
Kevin Barnes <kbarnes3@gmail.com>
2017-06-29 20:06:32 -0700

    Adding failing steady state test

--- python/tests/mock_evaluator.py
+++ python/tests/mock_evaluator.py
@@ -2,6 +2,10 @@ from tieronepointfive.enums import State, Transition
 
 
 class MockEvaluator:
+    def __init__(self, evaluation_func):
+        self.evaluation_func = evaluation_func
+
     def evaluate_tick(self, tick):
-        tick.complete(Transition.NO_SITES_REACHED, State.CONNECTION_FAILED)
+        transition, end_state = self.evaluation_func()
+        tick.complete(transition, end_state)
         return tick
--- python/tests/test_state_machine.py
+++ python/tests/test_state_machine.py
@@ -11,7 +11,7 @@ def _assert_ticks_equal(actual_tick, expected_tick):
     assert actual_tick.end_state == expected_tick.end_state
 
 
-def test_non_steady_ticks():
+def test_failing_ticks():
     def verify_tick_list(tick_list, expected_length):
         assert len(tick_list) == expected_length
 
@@ -27,7 +27,10 @@ def test_non_steady_ticks():
         for i in range(2, len(tick_list)):
             _assert_ticks_equal(tick_list[i], expected_other_tick)
 
-    mock_evaluator = MockEvaluator()
+    def evaluation_func():
+        return Transition.NO_SITES_REACHED, State.CONNECTION_FAILED
+
+    mock_evaluator = MockEvaluator(evaluation_func)
 
     start_tick = StateMachineTick(State.CONNECTION_WORKING, Transition.ALL_SITES_REACHED, State.CONNECTION_WORKING)
     machine = StateMachine(mock_evaluator, [start_tick])
@@ -36,4 +39,23 @@ def test_non_steady_ticks():
         tick_list = machine.evaluate()
         verify_tick_list(tick_list, i)
 
+
+def test_steady_state_ticks():
+    def verify_tick_list(tick_list):
+        assert len(tick_list) == 1
+
+        expected_tick = StateMachineTick(State.CONNECTION_WORKING, Transition.ALL_SITES_REACHED, State.CONNECTION_WORKING)
+        _assert_ticks_equal(tick_list[0], expected_tick)
+
+    def evaluation_func():
+        return Transition.ALL_SITES_REACHED, State.CONNECTION_WORKING
+
+    mock_evaluator = MockEvaluator(evaluation_func)
+
+    start_tick = StateMachineTick(State.CONNECTION_WORKING, Transition.ALL_SITES_REACHED, State.CONNECTION_WORKING)
+    machine = StateMachine(mock_evaluator, [start_tick])
+
+    for _ in range(1,15):
+        tick_list = machine.evaluate()
+        verify_tick_list(tick_list)
 
