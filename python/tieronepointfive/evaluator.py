e779653f4756980c77d6ad1b9f7581cce5ff6713
Kevin Barnes <kbarnes3@gmail.com>
2017-07-10 18:05:29 -0700

    Simplified evaluator

--- python/tieronepointfive/evaluator.py
+++ python/tieronepointfive/evaluator.py
@@ -1,16 +1,6 @@
 from .enums import State
 
 
-class MissingEvaluatorError(Exception):
-    def __init__(self, tick):
-        super().__init__("Missing helper for tick in state {0}".format(tick.start_state))
-
-
-class MissingHelper:
-    def evaluate(self, tick):
-        raise MissingEvaluatorError(tick)
-
-
 class Evaluator:
     def __init__(self, http_helper):
         self._evaluator_helpers = {
@@ -21,7 +11,7 @@ class Evaluator:
         if tick.is_complete():
             raise Exception("Can't evaluate an already completed tick")
 
-        evaluator = self._evaluator_helpers.get(tick.start_state, MissingHelper())
+        evaluator = self._evaluator_helpers[tick.is_complete]
 
         completed_tick = evaluator.evaluate(tick)
         return completed_tick
