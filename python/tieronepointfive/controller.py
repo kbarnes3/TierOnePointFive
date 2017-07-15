90bf0da9b351a368ab979046650405a7b1b9a748
Kevin Barnes <kbarnes3@gmail.com>
2017-07-14 18:08:44 -0700

    Adding printing to the controller

--- python/tieronepointfive/controller.py
+++ python/tieronepointfive/controller.py
@@ -3,6 +3,12 @@ from tieronepointfive.state_machine import StateMachine
 
 
 class Controller:
-    def __init__(self):
+    def __init__(self, print_to_stdout = True):
+        self._print_to_stdout = print_to_stdout
+
         evaluator = create_evaluator()
         self._state_machine = StateMachine(evaluator, None)
+
+    def _print(self, msg):
+        if self._print_to_stdout:
+            print(msg)
