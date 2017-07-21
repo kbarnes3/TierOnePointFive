b21b3b0183d937c1219ab73c0d6c4b498140583d
Kevin Barnes <kbarnes3@gmail.com>
2017-07-20 18:24:47 -0700

    Config files get created as needed

--- python/tieronepointfive/config.py
+++ python/tieronepointfive/config.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import shutil
+import sys
 
 from tieronepointfive.directories import DefaultDirectories
 
@@ -11,5 +13,13 @@ class Config:
         config_dir_path = Path(config_dir)
         config_file_path = config_dir_path / 'config.json'
         if not config_file_path.is_file():
-            config_dir_path.mkdir(parents=True, exist_ok=True)
-            config_file_path.touch(exist_ok=True)
+            self._create_new_config_file(config_dir_path, config_file_path)
+
+    @staticmethod
+    def _create_new_config_file(config_dir_path, config_file_path):
+        config_dir_path.mkdir(parents=True, exist_ok=True)
+        example_config_file = Path(__file__).parent / 'data' / 'config.example.json'
+        shutil.copy(example_config_file, config_file_path)
+
+        print('A new config file was created at:\n{0}\nPlease ensure it is correct before running TierOnePointFive'.format(str(config_file_path)))
+        sys.exit(1)
