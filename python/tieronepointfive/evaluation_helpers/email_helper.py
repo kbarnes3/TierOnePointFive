b2ab54f446769fec5b989e5f8921d489e15cfb3e
Kevin Barnes <kbarnes3@gmail.com>
2017-07-23 18:14:45 -0700

    Email now has names and emails

--- python/tieronepointfive/evaluation_helpers/email_helper.py
+++ python/tieronepointfive/evaluation_helpers/email_helper.py
@@ -12,19 +12,43 @@ class EmailHelper:
 
         # Create message container - the correct MIME type is multipart/alternative.
         msg = MIMEMultipart('alternative')
-        msg['From'] = email_config.from_name
-        msg['To'] = email_config.to_names
+        self._build_from_field(msg, email_config)
+        self._build_to_field(msg, email_config)
         msg['Subject'] = '[Tier 1.5] Network Event Report'
 
         self._prepare_email_body(msg, tick_list)
+
+        to_emails = [to.email for to in email_config.to]
 
         # Send the message via local SMTP server.
         s = smtplib.SMTP_SSL(email_config.server_address, email_config.server_port)
         s.login(email_config.login, email_config.password)
         # sendmail function takes 3 arguments: sender's address, recipient's address
         # and message to send - here it is sent as one string.
-        s.sendmail(email_config.from_name, email_config.to_names, msg.as_string())
+        s.sendmail(email_config.sender.email, to_emails, msg.as_string())
         s.quit()
+
+    def _build_from_field(self, msg, email_config):
+        sender_config = email_config.sender
+        msg['From'] = self._build_rfc_822_address(sender_config)
+
+    def _build_to_field(self, msg, email_config):
+        to_config = email_config.to
+        for i in range(0, len(to_config)):
+            address = self._build_rfc_822_address(to_config[i])
+            if i == 0:
+                to_field = address
+            else:
+                to_field += ', {0}'.format(address)
+
+        msg['To'] = to_field
+
+    @staticmethod
+    def _build_rfc_822_address(config):
+        if hasattr(config, 'name'):
+            return '{0} <{1}>'.format(config.name, config.email)
+        else:
+            return config.email
 
     def _prepare_email_body(self, msg, tick_list):
         opening = "Tier 1.5 detected a network event. The details and actions performed are below."
